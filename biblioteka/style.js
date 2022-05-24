87fe632e7e7d4aec091cb6886d5c3e687a0b52c8
Natali <m.n.a1881@mail.ru>
2022-05-24 15:27:11 +0400

    php

--- biblioteka/style.js
+++ biblioteka/style.js
@@ -3,5 +3,20 @@ const form = document.querySelector('form')
 
 form.addEventListener('submit', (e) => {
     e.preventDefault()
-    
+
+    const files = document.querySelector('[type=file]').files;
+    const formData = new FormData();
+
+    for (let i = 0; i < files.length; i++){
+        let file = files[i]
+
+        formData.append('files[]', file)
+    }
+
+    fetch(url, {
+        method: 'POST',
+        body: formData,
+      }).then((response) => {
+        console.log(response)
+      })
 })
