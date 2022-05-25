9ba13f0f2ffb64733fd568453f0ba85e7375aff3
Natali <m.n.a1881@mail.ru>
2022-05-25 17:48:59 +0400

    php

--- biblioteka/process.php
+++ biblioteka/process.php
@@ -1,1 +1,11 @@
-<?php print_r($_FILES); ?>
+<?php print_r($_FILES);
+if ($_SERVER['REQUEST_METHOD'] === 'POST'){
+    if(isset($_FILES['files'])){
+        $errors =[];
+        $path = 'uploads/';
+        $extensions = ['txt'];
+
+        $all_files = count($_FILES['files']['tmp_name']);
+    }
+}
+?>
